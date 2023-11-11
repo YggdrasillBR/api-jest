19e448b1cafcbbfdcf15f0f5228fb7135b358e12
YggdrasillBR <ferreirac.lucas1@gmail.com>
2023-11-10 23:32:15 -0300

    configs I

---
+++ deps.ts
@@ -0,0 +1,25 @@
+//
+"dependencies": {
+    "dotenv": "8.2.0",
+    "fp-ts": "2.9.5",
+    "module-alias": "2.2.2"
+  },
+  "devDependencies": {
+    "@types/jest": "26.0.22",
+    "@types/module-alias": "2.0.0",
+    "@types/node": "14.14.41",
+    "@typescript-eslint/eslint-plugin": "4.21.0",
+    "@typescript-eslint/parser": "4.21.0",
+    "eslint": "^7.12.1",
+    "eslint-config-standard": "16.0.2",
+    "eslint-plugin-import": "^2.22.1",
+    "eslint-plugin-jest": "24.3.5",
+    "eslint-plugin-node": "^11.1.0",
+    "eslint-plugin-promise": "^4.2.1",
+    "husky": "6.0.0",
+    "jest": "26.6.3",
+    "nodemon": "2.0.7",
+    "sucrase": "3.17.1",
+    "ts-jest": "26.5.4",
+    "typescript": "4.2.4"
+  }//
